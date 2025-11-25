ca7bdc99f9454cff8658a736d095ee63f21e6de9
OlegDanilyuk <OlegDanilyuk@kavkaz.ttk.ru>
2025-11-25 17:52:26 +0300

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+final class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
+}
---
+++ phpinsights.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
+
+return [
+    'preset' => 'laravel',
+    'exclude' => [
+        'database/*',
+    ],
+    'config' => [
+        LineLengthSniff::class => [
+            'lineLimit' => 120,
+            'absoluteLineLimit' => 160,
+        ],
+    ],
+];
